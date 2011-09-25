e86c846114929cf976d74c2135130b749f7ec195
dalia9123@gmail.com <dalia9123@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-09-25 15:06:50 +0000

    android tutorial

---
+++ trunk/Dalia El Badawi/Tutorial/src/com/example/helloandroid/TutorialActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+Author: Dalia El Badawi
+Created: 24/09/2011
+ *
+**/
+
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
