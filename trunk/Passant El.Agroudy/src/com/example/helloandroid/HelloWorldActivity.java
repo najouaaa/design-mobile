e608620e5fbbe30a568d9dc2e4f423665c8c1e7d
passant.elagroudy@gmail.com <passant.elagroudy@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-09-27 22:52:01 +0000

    Final Version of Hellp Android Assignment

--- trunk/Passant El.Agroudy/src/com/example/helloandroid/HelloWorldActivity.java
+++ trunk/Passant El.Agroudy/src/com/example/helloandroid/HelloWorldActivity.java
@@ -2,12 +2,16 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //Object o=null;
+        //o.toString();
+        setContentView(R.layout.main); 
+        
     }
 }
