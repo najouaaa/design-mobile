9cb516f12013aa478de23aee309384b872903997
kamasheto <kamasheto@58fc6eb2-5516-efc4-609f-126101431d35>
2011-09-28 18:43:16 +0000

    Cleaned up code.

--- trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
+++ trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
@@ -1,10 +1,3 @@
-/**
- * 
-Author: Dalia El Badawi
-Created: 24/09/2011
- *
-**/
-
 package com.horoscope;
 
 import android.app.Activity;
@@ -12,33 +5,34 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * @author Dalia El Badawi
+ **/
 public class HoroscopeDetails extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       
-        int pos = (getIntent().getIntExtra("pos",1))+1;
-        String name = getIntent().getStringExtra("title");
-        
-        setContentView(R.layout.linear);
-        setImageAndText(pos,name);
-    }
-    
-    public void setImageAndText(int pos,String title){
-    	ImageView horoscope = (ImageView) findViewById(R.id.horoscope_image);
-        horoscope.setImageDrawable(getResources().getDrawable(R.drawable.icon));//getResources().getIdentifier("image_"+name, "string","com.horoscope")));
-       
-        TextView t = (TextView) findViewById(R.id.title);
-        t.setText(title);
-        
-        
-        TextView text1 = (TextView) findViewById(R.id.text1);
-        text1.setText(getResources().getString(getResources().getIdentifier("a"+pos+"1", "string","com.horoscope")));
-        
-        TextView text2 = (TextView) findViewById(R.id.text2);
-        text2.setText(getResources().getString(getResources().getIdentifier("a"+pos+"2", "string","com.horoscope")));
-    	
-    }
-    
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		int pos = (getIntent().getIntExtra("pos", 1)) + 1;
+		String name = getIntent().getStringExtra("title");
+
+		setContentView(R.layout.linear);
+		setImageAndText(pos, name);
+	}
+
+	public void setImageAndText(int pos, String title) {
+		ImageView horoscope = (ImageView) findViewById(R.id.horoscope_image);
+		horoscope.setImageDrawable(getResources().getDrawable(R.drawable.icon));// getResources().getIdentifier("image_"+name,
+		// "string","com.horoscope")));
+
+		TextView t = (TextView) findViewById(R.id.title);
+		t.setText(title);
+
+		TextView text1 = (TextView) findViewById(R.id.text1);
+		text1.setText(getResources().getString(getResources().getIdentifier("a" + pos + "1", "string", "com.horoscope")));
+
+		TextView text2 = (TextView) findViewById(R.id.text2);
+		text2.setText(getResources().getString(getResources().getIdentifier("a" + pos + "2", "string", "com.horoscope")));
+	}
 }
--- trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
+++ trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
@@ -1,49 +1,39 @@
 package com.horoscope;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.view.*;
+import android.widget.AdapterView.OnItemClickListener;
 
+/**
+ * @author Dina
+ */
 public class HoroscopesList extends ListActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    	String[] countries = getResources().getStringArray(R.array.countries_array);
-    	setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, countries));
-    	
-    	  ListView lv = getListView();
-    	  lv.setTextFilterEnabled(true);
+		String[] countries = getResources().getStringArray(R.array.countries_array);
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, countries));
 
-    	  lv.setOnItemClickListener(new OnItemClickListener() {
-    	    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    	    	 //switch(position) {
-    	    	 //case 0:
-    	    		 Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
-    	    		 
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
 
-    	    		 i.putExtra("pos", position);//
-    	    		 i.putExtra("title",((TextView) view).getText());
-    	    		 
-    	    		 startActivity(i);
-    	    	   // }	
-    	    }
-    	  });
-    }
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
+
+				i.putExtra("pos", position);
+				i.putExtra("title", ((TextView) view).getText());
+
+				startActivity(i);
+			}
+		});
+	}
 }
