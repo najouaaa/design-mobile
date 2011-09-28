011601fac5939e3ca8f7465d4d310f790ea0b722
dalia9123 <dalia9123@gmail.com>
2011-09-28 18:21:56 +0000

    adjusted linear layout

--- Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
+++ Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
@@ -17,24 +17,28 @@ public class HoroscopeDetails extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int x = R.drawable.icon; 
-        int y = getIntent().getIntExtra("text1", 0);
-        int z = getIntent().getIntExtra("text2", 0);
-        
+       
+        int pos = (getIntent().getIntExtra("pos",1))+1;
+        String name = getIntent().getStringExtra("title");
         
         setContentView(R.layout.linear);
-        setImageAndText(x,y,z);
+        setImageAndText(pos,name);
     }
     
-    public void setImageAndText(int x,int y,int z){
+    public void setImageAndText(int pos,String title){
     	ImageView horoscope = (ImageView) findViewById(R.id.horoscope_image);
-        horoscope.setImageDrawable(getResources().getDrawable(x));
+        horoscope.setImageDrawable(getResources().getDrawable(R.drawable.icon));//getResources().getIdentifier("image_"+name, "string","com.horoscope")));
+       
+        TextView t = (TextView) findViewById(R.id.title);
+        t.setText(title);
+        
         
         TextView text1 = (TextView) findViewById(R.id.text1);
-        text1.setText(getResources().getString(y));
+        text1.setText(getResources().getString(getResources().getIdentifier("a"+pos+"1", "string","com.horoscope")));
         
         TextView text2 = (TextView) findViewById(R.id.text2);
-        text2.setText(getResources().getString(z));
+        text2.setText(getResources().getString(getResources().getIdentifier("a"+pos+"2", "string","com.horoscope")));
     	
     }
+    
 }
--- Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
+++ Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
@@ -1,5 +1,14 @@
 package com.horoscope;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.view.*;
 
 public class HoroscopesList extends ListActivity {
@@ -23,13 +33,16 @@ public class HoroscopesList extends ListActivity {
 
     	  lv.setOnItemClickListener(new OnItemClickListener() {
     	    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    	    	 switch(position) {
-    	    	 case 0:
+    	    	 //switch(position) {
+    	    	 //case 0:
     	    		 Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
-    	    		 i.putExtra("text1", R.string.horse1);
-    	    		 i.putExtra("text2", R.string.horse2);
+    	    		 
+
+    	    		 i.putExtra("pos", position);//
+    	    		 i.putExtra("title",((TextView) view).getText());
+    	    		 
     	    		 startActivity(i);
-    	    	    }	
+    	    	   // }	
     	    }
     	  });
     }
