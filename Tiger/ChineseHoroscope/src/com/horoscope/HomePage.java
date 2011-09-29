3d44ebfe2726f3a3a5a5d3337b84e36a96877eac
kamasheto <kamasheto@58fc6eb2-5516-efc4-609f-126101431d35>
2011-09-29 17:46:55 +0000

    Modified cancel key behaviour (now takes you back to view 1) Changed first view of tabs to Grid view
    Update issue 42
    Status started

--- Tiger/ChineseHoroscope/src/com/horoscope/HomePage.java
+++ Tiger/ChineseHoroscope/src/com/horoscope/HomePage.java
@@ -1,6 +1,5 @@
 package com.horoscope;
 
-
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -8,49 +7,47 @@ import android.os.Bundle;
 import android.widget.TabHost;
 
 /**
- * 
  * @author Rana
- *
  */
-public class HomePage extends TabActivity{
+public class HomePage extends TabActivity {
+
+	TabHost tabHost;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_view);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab_view);
 
-        Resources res = getResources(); // Resource object to get Drawables
-        TabHost tabHost = getTabHost();  // The activity TabHost
-        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
-        Intent intent;  // Reusable Intent for each tab
-
-        // Create an Intent to launch the horoscopesList activity
-        intent = new Intent().setClass(this, HoroscopesList.class);
-        // Initialize tab spec
-        spec = tabHost.newTabSpec("list").setIndicator("",
-                res.getDrawable(R.drawable.list_view_state))
-            .setContent(intent);
-
-        // Add tabspec to the TabHost
-        tabHost.addTab(spec);
-      
-        intent = new Intent().setClass(this, HoroscopeGrid.class);
-
-        // Same as done above is repeated for the rest of the views; grid and search
-        spec = tabHost.newTabSpec("grid").setIndicator("",
-                res.getDrawable(R.drawable.grid_view_state))
-            .setContent(intent);
-        tabHost.addTab(spec);
-      
-        intent = new Intent().setClass(this, HoroscopeSearch.class);
+		Resources res = getResources(); // Resource object to get Drawables
+		tabHost = getTabHost(); // The activity TabHost
+		TabHost.TabSpec spec; // Resusable TabSpec for each tab
+		Intent intent; // Reusable Intent for each tab
 
 
-        spec = tabHost.newTabSpec("search").setIndicator("",
-                res.getDrawable(R.drawable.search_view_state))
-            .setContent(intent);
-        tabHost.addTab(spec);
-        
-        //Set the open tab to be the first tab which is the list view
-        tabHost.setCurrentTab(0);
-    }
+		intent = new Intent().setClass(this, HoroscopeGrid.class);
+		// Same as done below is repeated for the rest of the views; grid and
+		// search
+		spec = tabHost.newTabSpec("grid").setIndicator("", res.getDrawable(R.drawable.grid_view_state)).setContent(intent);
+		tabHost.addTab(spec);
+		
+		// Create an Intent to launch the horoscopesList activity
+		intent = new Intent().setClass(this, HoroscopesList.class);
+		// Initialize tab spec
+		spec = tabHost.newTabSpec("list").setIndicator("", res.getDrawable(R.drawable.list_view_state)).setContent(intent);
+		// Add tabspec to the TabHost
+		tabHost.addTab(spec);
 
+
+		intent = new Intent().setClass(this, HoroscopeSearch.class);
+		spec = tabHost.newTabSpec("search").setIndicator("", res.getDrawable(R.drawable.search_view_state)).setContent(intent);
+		tabHost.addTab(spec);
+
+		// Set the open tab to be the first tab which is the list view
+		// tabHost.setCurrentTab(0);
+		switchTab(1);
+	}
+
+	public void switchTab(int tab) {
+		tabHost.setCurrentTab(tab);
+	}
 }
