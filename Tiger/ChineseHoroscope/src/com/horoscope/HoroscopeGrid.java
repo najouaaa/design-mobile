46016fb1c3168f9b5d3d47aacb58000e01fd2cf4
mhmgad <mhmgad@gmail.com>
2011-09-29 21:59:36 +0000

    Linked to linear view

--- Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeGrid.java
+++ Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeGrid.java
@@ -1,11 +1,12 @@
 package com.horoscope;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.ImageView;
 import android.widget.AdapterView.OnItemClickListener;
 
 /**
@@ -17,14 +18,26 @@ public class HoroscopeGrid extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grid);
-
+		/*
+		 * Adding the grid for the view
+		 */
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(this));
+	
 
 		gridview.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				Toast.makeText(HoroscopeGrid.this, "" + position, Toast.LENGTH_SHORT).show();
+				
+				/*
+				 * Link auto-complete textbox to list of horoscopes
+				 */
+				
+				Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
+				i.putExtra("pos", position);
+				i.putExtra("title", ((String)((ImageView)v).getTag()).toString());
+				startActivity(i);
+				
 			}
 		});
 	}
