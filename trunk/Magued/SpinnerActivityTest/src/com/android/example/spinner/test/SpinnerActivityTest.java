decd1e2e86f909cb535a43e7166782db784c664a
magued.george1990@gmail.com <magued.george1990@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-01 20:37:48 +0000

    Added the 1st task "HelloAndroid" in a folder Added the 2nd and the 3rd tasks "HelloAndroidTest" & "SpinnerActivityTest"

---
+++ trunk/Magued/SpinnerActivityTest/src/com/android/example/spinner/test/SpinnerActivityTest.java
@@ -0,0 +1,130 @@
+package com.android.example.spinner.test;
+
+import com.android.example.spinner.SpinnerActivity;
+
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.view.KeyEvent;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+public class SpinnerActivityTest extends
+		ActivityInstrumentationTestCase2<SpinnerActivity> {
+
+	private SpinnerActivity mActivity;
+	private Spinner mSpinner;
+	private SpinnerAdapter mPlanetData;
+	  
+	 public SpinnerActivityTest() {
+		    super("com.android.example.spinner", SpinnerActivity.class);
+		  } 
+	 
+	 @Override
+	  protected void setUp() throws Exception {
+	    super.setUp();
+
+	    setActivityInitialTouchMode(false);
+
+	    mActivity = getActivity();
+
+	    mSpinner =
+	      (Spinner) mActivity.findViewById(
+	        com.android.example.spinner.R.id.Spinner01
+	      );
+
+	      mPlanetData = mSpinner.getAdapter();
+
+	  } 
+	 
+	 public static final int ADAPTER_COUNT = 9;
+	 
+	 public void testPreConditions() {
+		    assertTrue(mSpinner.getOnItemSelectedListener() != null);
+		    assertTrue(mPlanetData != null);
+		    assertEquals(mPlanetData.getCount(),ADAPTER_COUNT);
+		  } 
+	
+	 public static final int INITIAL_POSITION = 0;
+	 public static final int TEST_POSITION = 5;
+	 private String mSelection;
+	 private int mPos;
+	 
+	 public void testSpinnerUI() {
+
+		    mActivity.runOnUiThread(
+		      new Runnable() {
+		        public void run() {
+		          mSpinner.requestFocus();
+		          mSpinner.setSelection(INITIAL_POSITION);
+		        } 
+		      } 
+		    );
+		    
+		    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+		    for (int i = 1; i <= TEST_POSITION; i++) {
+		      this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+		    } 
+
+		    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+		    
+		    mPos = mSpinner.getSelectedItemPosition();
+		    mSelection = (String)mSpinner.getItemAtPosition(mPos);
+		    TextView resultView =
+		      (TextView) mActivity.findViewById(
+		        com.android.example.spinner.R.id.SpinnerResult
+		      );
+
+		    String resultText = (String) resultView.getText();
+
+		    assertEquals(resultText,mSelection);
+
+		  } 
+	 
+	 public static final int TEST_STATE_DESTROY_POSITION = 2;
+	 public static final String TEST_STATE_DESTROY_SELECTION = "Earth";
+	 
+	 public void testStateDestroy() {
+		    mActivity.setSpinnerPosition(TEST_STATE_DESTROY_POSITION);
+		    mActivity.setSpinnerSelection(TEST_STATE_DESTROY_SELECTION);
+		    
+		    mActivity.finish();
+		    mActivity = this.getActivity();
+		    
+		    int currentPosition = mActivity.getSpinnerPosition();
+		    String currentSelection = mActivity.getSpinnerSelection();
+		    
+		    assertEquals(TEST_STATE_DESTROY_POSITION, currentPosition);
+		    assertEquals(TEST_STATE_DESTROY_SELECTION, currentSelection);
+		    
+		}
+	    
+	 public static final int TEST_STATE_PAUSE_POSITION = 4;
+	 public static final String TEST_STATE_PAUSE_SELECTION = "Jupiter";
+	 
+	 @UiThreadTest
+	    public void testStatePause() {
+		 
+		 	Instrumentation mInstr = this.getInstrumentation();
+		 	
+		 	mActivity.setSpinnerPosition(TEST_STATE_PAUSE_POSITION);
+		    mActivity.setSpinnerSelection(TEST_STATE_PAUSE_SELECTION);
+		    
+		    mInstr.callActivityOnPause(mActivity);
+		    
+		    mActivity.setSpinnerPosition(0);
+		    mActivity.setSpinnerSelection("");
+		    
+		    mInstr.callActivityOnResume(mActivity);
+		    
+		    int currentPosition = mActivity.getSpinnerPosition();
+			String currentSelection = mActivity.getSpinnerSelection();
+			
+			assertEquals(TEST_STATE_PAUSE_POSITION,currentPosition);
+		    assertEquals(TEST_STATE_PAUSE_SELECTION,currentSelection);
+	 }
+	 
+		    
+		    
+}
