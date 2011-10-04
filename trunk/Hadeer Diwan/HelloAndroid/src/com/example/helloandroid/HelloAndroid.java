990c8a90922f2cb5a5ca788fb3fb33819481cea4
hadeer.diwan@gmail.com <hadeer.diwan@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-04 14:20:14 +0000

    updated the file to follow the Hello wrold Tutorial.

--- trunk/Hadeer Diwan/HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
+++ trunk/Hadeer Diwan/HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
@@ -8,9 +8,12 @@ public class HelloAndroid extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, my Chinese name is <Dong Hui dan> and my Chinese horoscope is <Horse>");
-        setContentView(tv);
+        setContentView(R.layout.main);
+        //TextView tv = new TextView(this);
+        //tv.setText("Hello, Android");
+        //setContentView(tv);
+        //tv.setText("Hello, my Chinese name is <Dong Hui dan> and my Chinese horoscope is <Horse>");
+        //setContentView(tv);
     }
+ 
 }
