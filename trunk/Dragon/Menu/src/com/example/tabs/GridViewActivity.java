7185bf493c17bb9c836504ebe96ea8279eee88b6
ahmed.abourayax@gmail.com <ahmed.abourayax@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-09 10:23:25 +0000

    removing the display text part

--- trunk/Dragon/Menu/src/com/example/tabs/GridViewActivity.java
+++ trunk/Dragon/Menu/src/com/example/tabs/GridViewActivity.java
@@ -50,7 +50,7 @@ public class GridViewActivity extends Activity {
     	        		startActivity(intent);
     	        	break;
     	        	}
-    	        	 Toast.makeText(GridViewActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+    	        	 //Toast.makeText(GridViewActivity.this, "" + position, Toast.LENGTH_SHORT).show();
     	            
     	        }
     	    });
