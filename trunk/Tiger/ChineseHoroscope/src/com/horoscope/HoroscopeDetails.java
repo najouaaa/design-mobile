ab63eea758ceba330553f7bc256f98220a8c36e5
Rana.morsy90@gmail.com <Rana.morsy90@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-10 21:51:09 +0000

    Added Next/prev buttons

--- trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
+++ trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeDetails.java
@@ -1,7 +1,10 @@
 package com.horoscope;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,12 +13,18 @@ import android.widget.TextView;
  **/
 public class HoroscopeDetails extends Activity {
 	/** Called when the activity is first created. */
+
+	int nextCount;
+	String[] horoscopes;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		int pos = (getIntent().getIntExtra("pos", 1)) + 1; //position of item in list
-		String name = getIntent().getStringExtra("title"); //text of item
+		nextCount = 0;
+		horoscopes = getResources().getStringArray(R.array.horoscope_array);
+		int pos = (getIntent().getIntExtra("pos", 0)); // position of item
+		// in list
+		String name = getIntent().getStringExtra("title"); // text of item
 
 		setContentView(R.layout.linear);
 		setImageAndText(pos, name);
@@ -28,19 +37,72 @@ public class HoroscopeDetails extends Activity {
 	 * @param title
 	 */
 	public void setImageAndText(int pos, String title) {
-		//horoscope image
+		// horoscope image
 		ImageView horoscope = (ImageView) findViewById(R.id.horoscope_image);
-		horoscope.setImageDrawable(getResources().getDrawable(getResources().getIdentifier(title.toLowerCase()+"_zodiac","drawable","com.horoscope")));
-		//name of horoscope
+		horoscope.setImageDrawable(getResources().getDrawable(
+				getResources().getIdentifier(title.toLowerCase() + "_zodiac",
+						"drawable", "com.horoscope")));
+		// name of horoscope
 		TextView t = (TextView) findViewById(R.id.title);
 		t.setText(title);
-		
-		//first part of description, id is a+position of item in list+1
+
+		// first part of description, id is a+position of item in list+1
 		TextView text1 = (TextView) findViewById(R.id.text1);
-		text1.setText(getResources().getString(getResources().getIdentifier("a" + pos + "1", "string", "com.horoscope")));
-		
-		//second part of description id is a+position of item in list+2
+		text1.setText(getResources().getString(
+				getResources().getIdentifier("a" + pos + "1", "string",
+						"com.horoscope")));
+
+		// second part of description id is a+position of item in list+2
 		TextView text2 = (TextView) findViewById(R.id.text2);
-		text2.setText(getResources().getString(getResources().getIdentifier("a" + pos + "2", "string", "com.horoscope")));
+		text2.setText(getResources().getString(
+				getResources().getIdentifier("a" + pos + "2", "string",
+						"com.horoscope")));
+	}
+
+	/**
+	 * @author Rana
+	 * 
+	 * @param view
+	 */
+	public void showNextHoroscope(View view) {
+		nextCount++;
+		int pos = (getIntent().getIntExtra("pos", 0)) + nextCount; // position
+																	// of item
+
+		
+		String name = horoscopes[pos]; // Since next count starts by 1
+		setContentView(R.layout.linear);
+		setImageAndText(pos, name);
+		if (pos == (horoscopes.length - 1)) {
+			findViewById(R.id.nextButton).setVisibility(
+					android.view.View.INVISIBLE);
+		}else
+		{
+			findViewById(R.id.nextButton).setVisibility(android.view.View.VISIBLE);
+		}
+	}
+
+	/**
+	 * @author Rana
+	 * @param view
+	 */
+	public void showPrevHoroscope(View view) {
+		nextCount--;
+		int pos = (getIntent().getIntExtra("pos", 0)) + nextCount; // position
+																	// of item
+
+		
+		String name = horoscopes[pos]; // Since next count starts by 1
+		setContentView(R.layout.linear);
+		setImageAndText(pos, name);
+		if (pos == 0) {
+			findViewById(R.id.prevButton).setVisibility(
+					android.view.View.INVISIBLE);
+		}
+		else
+		{
+			findViewById(R.id.prevButton).setVisibility(android.view.View.VISIBLE);
+		}
+
 	}
 }
