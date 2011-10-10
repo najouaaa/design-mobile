b1cfa69c4b24066f44042b553ecb29e0b2d029da
hadeer.diwan <hadeer.diwan@gmail.com>
2011-10-10 22:17:52 +0000

    Added a next and back button.

--- Dragon/Menu/src/com/example/tabs/LinearActivity.java
+++ Dragon/Menu/src/com/example/tabs/LinearActivity.java
@@ -2,13 +2,21 @@ package com.example.tabs;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class LinearActivity extends Activity {
-	
+public class LinearActivity extends Activity implements View.OnClickListener {
+	// defining handler classes as a private inner class
+
 	public static int the_id=0;
-	
+	TextView content1 ;
+	TextView content2 ;
+	ImageView myImg;
+	Button button1;
+	Button button2;
 	/** Called when the activity is first created. */
     
 	@Override
@@ -16,10 +24,15 @@ public class LinearActivity extends Activity {
         
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.linear);
-		TextView content1 = (TextView)findViewById(R.id.txt1);
-		TextView content2 = (TextView)findViewById(R.id.txt2);
-		ImageView myImg = (ImageView)findViewById(R.id.img);
-
+		 content1 = (TextView)findViewById(R.id.txt1);
+		 content2 = (TextView)findViewById(R.id.txt2);
+		 myImg = (ImageView)findViewById(R.id.img);
+		 button1 =(Button)findViewById(R.id.first_button);
+		 button2 =(Button)findViewById(R.id.second_button);
+		 button1.setClickable(true);
+	     button2.setClickable(true);
+	     button1.setOnClickListener(this);
+         button2.setOnClickListener(this);
         switch(the_id)
         {
         	case 1:
@@ -48,5 +61,54 @@ public class LinearActivity extends Activity {
         		myImg.setImageResource(R.drawable.rabbit);
         	break;
         }
-    }
+    
+       
+		
+	}
+	public void onClick(View v) {
+		if(button1.isPressed())
+		{
+		the_id--;
+		if(the_id<0)
+			the_id=4;}
+		else if(button2.isPressed())
+		{
+			the_id++;
+			if(the_id>4)
+				the_id=0;
+		}
+		switch(the_id)
+        {
+        	case 1:
+        		content1.setText(R.string.HorseName);
+        		content2.setText(R.string.HorseDescription);
+        		myImg.setImageResource(R.drawable.horse);
+        	break;
+        	case 0:
+        		content1.setText(R.string.DragonName);
+        		content2.setText(R.string.DragonDescription);
+        		myImg.setImageResource(R.drawable.dragon);
+        	break;
+        	case 3:
+        		content1.setText(R.string.SnakeName);
+        		content2.setText(R.string.SnakeDescription);
+        		myImg.setImageResource(R.drawable.snake);
+        	break;
+        	case 4:
+        		content1.setText(R.string.SheepName);
+        		content2.setText(R.string.SheepDescription);
+        		myImg.setImageResource(R.drawable.sheep);
+        	break;
+        	case 2:
+        		content1.setText(R.string.RabbitName);
+        		content2.setText(R.string.RabbitDescription);
+        		myImg.setImageResource(R.drawable.rabbit);
+        	break;
+        }
+
+	}
+
+	    
+	  
 }
+	
