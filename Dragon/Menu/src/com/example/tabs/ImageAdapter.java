b7dca9930677f03c42ef88f35b0a0af9585bfb2a
ahmed.abourayax <ahmed.abourayaX@gmail.com>
2011-10-10 09:02:15 +0000

    Adding Text for each image in the grid View

--- Dragon/Menu/src/com/example/tabs/ImageAdapter.java
+++ Dragon/Menu/src/com/example/tabs/ImageAdapter.java
@@ -1,11 +1,13 @@
 package com.example.tabs;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ImageAdapter extends BaseAdapter{
 	   private Context mContext;
@@ -28,23 +30,32 @@ public class ImageAdapter extends BaseAdapter{
 
 	    // create a new ImageView for each item referenced by the Adapter
 	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ImageView imageView;
+	       
+	        View row;
 	        if (convertView == null) {  // if it's not recycled, initialize some attributes
-	            imageView = new ImageView(mContext);
-	            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	            imageView.setPadding(8, 8, 8, 8);
+	        	 ImageView imageView = new ImageView(mContext);
+	            LayoutInflater inflater = (LayoutInflater)
+	            mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	            row=inflater.inflate(R.layout.single_grid_item, parent, false);
+	            TextView label=(TextView)row.findViewById(R.id.image_name);
+	            label.setText(mTexts[position]);
+	            imageView=(ImageView)row.findViewById(R.id.album_image);
+	            imageView.setImageResource(mThumbIds[position]);
 	        } else {
-	            imageView = (ImageView) convertView;
+	            row = (View) convertView;
 	        }
 
-	        imageView.setImageResource(mThumbIds[position]);
-	        return imageView;
+	       
+	        return row;
 	    }
 
 	    // references to our images
 	    private Integer[] mThumbIds = {
 	            R.drawable.dragon, R.drawable.horse,
 	            R.drawable.rabbit, R.drawable.snake,R.drawable.sheep,
+	    };
+	    private String[] mTexts = {
+	            "dragon", "horse",
+	            "rabbit", "snake","sheep",
 	    };
 	}
