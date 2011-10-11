246b3fe6e9f69931fcc511c2fffa5bdc3ffd2b7d
nada.adly <nada.adly@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 19:50:43 +0000

    Commit the spinner and ViewStub in the relative layout

--- trunk/Rabbit/TabView/src/com/example/helloViews/RelativeViewActivity.java
+++ trunk/Rabbit/TabView/src/com/example/helloViews/RelativeViewActivity.java
@@ -4,29 +4,88 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RelativeViewActivity extends Activity {
+	int Pos;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.relative_view);
+
+	    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.horoscopes_array, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner.setAdapter(adapter);
+	    
+	    spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());		
+		
 	}
-
-
-	/** Method called when buttons are clicked */
-	public void onClickButton(View v) {
-		// Get the button that generated the click event 
-		Button b = (Button) v;
 	
+	
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+		
+	    public void onItemSelected(AdapterView<?> parent,
+	        View view, int pos, long id) {
+	    	
+	    	 if(RelativeViewActivity.this.Pos != pos){
+	   	           
+	    	
+	    		 ViewStub stub1 = (ViewStub) findViewById(R.id.stub);
+	    		 String animalName = parent.getItemAtPosition(pos).toString();
+	   
+	    		 if(stub1 != null)  {
+	    			 View inflated = stub1.inflate();
+	    		 }
+	    	
+	    		 setAnimal(animalName);
+	    		 RelativeViewActivity.this.Pos = pos;
+	    	 	}// end if
+	   	    }// end onItemSelected
 
-		// Generate intent to move between activities
-		Intent mIntent = new Intent(getApplicationContext(),
-				HoroscopeInfoActivity.class);
-		// Pass variable to detailed view activity using the intent
-		mIntent.putExtra(TabViewActivity.ANIMAL_KEY, b.getText());
-		// Start the new activity
-		startActivity(mIntent);
+	    
+	    public void setAnimal(String animal) {
+	    	
+	    	ImageView iv = (ImageView)findViewById(R.id.icon);
+	    	TextView textView1 = (TextView)findViewById(R.id.textview1);
+	    	TextView textView2 = (TextView)findViewById(R.id.textview2);
+	    	
+	    	if(animal.equalsIgnoreCase(getString(R.string.h_rabbit))) {
+	    		iv.setImageResource(R.drawable.rabbit);
+	    		textView1.setText(R.string.h_rabbit);
+	    		textView2.setText(R.string.rabbit_desc);
+	    	} else if(animal.equalsIgnoreCase(getString(R.string.h_dragon))) {
+	    		iv.setImageResource(R.drawable.dragon);
+	    		textView1.setText(R.string.h_dragon);
+	    		textView2.setText(R.string.dragon_desc);    		
+	    	} else if(animal.equalsIgnoreCase(getString(R.string.h_ox))) {
+	    		iv.setImageResource(R.drawable.ox);
+	    		textView1.setText(R.string.h_ox);
+	    		textView2.setText(R.string.ox_desc);    		
+	    	} else if(animal.equalsIgnoreCase(getString(R.string.h_tiger))) {
+	    		iv.setImageResource(R.drawable.tiger);
+	    		textView1.setText(R.string.h_tiger);
+	    		textView2.setText(R.string.tiger_desc);    		
+	    	} else if(animal.equalsIgnoreCase(getString(R.string.h_rat))) {
+	    		iv.setImageResource(R.drawable.rat);
+	    		textView1.setText(R.string.h_rat);
+	    		textView2.setText(R.string.rat_desc);     		
+	    	}
+	    	
+	    }// end setAnimal
+	    
+	    public void onNothingSelected(AdapterView parent) {
+	      // Do nothing.
+	    }// end onNothingSelected
+	}// end MyOnItemSelectedListener
 
-	}// end onClickButton
-}
+}// end RelativeViewActivity
