e51e75216aa3ceeee2f52dba74773669b0ed65e7
dina.e.helal <dina.e.helal@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 16:39:27 +0000

    code clean up

--- trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
+++ trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopesList.java
@@ -24,10 +24,9 @@ import android.widget.AdapterView.OnItemClickListener;
  */
 public class HoroscopesList extends Activity {
     /** Called when the activity is first created. */
-    // I use HashMap arraList which takes objects
     private ArrayList <HashMap<String, Object>> horoscopes_hashmap;
-    private static final String TITLEKEY = "bookname";
-    private static final String IMGKEY = "iconfromraw";
+    private static final String TITLEKEY = "horoscopetitle";
+    private static final String IMGKEY = "icon";
     private static final String RATINGKEY = "ratings";
      
     @Override
@@ -36,7 +35,6 @@ public class HoroscopesList extends Activity {
         setContentView(R.layout.listview);
         ListView listView = (ListView)findViewById(R.id.list);
         horoscopes_hashmap = new ArrayList<HashMap<String,Object>>();
-        HashMap<String, Object> hm;
         final String[] horoscopes = getResources().getStringArray(R.array.horoscope_array);
         
         listView.setOnItemClickListener(new OnItemClickListener() {
@@ -56,81 +54,19 @@ public class HoroscopesList extends Activity {
 				startActivity(i);
 			}
 		});
-	
-        
-        //With the help of HashMap add Key, Values of Book, like name,price and icon path
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Rat");
-        hm.put(IMGKEY, R.drawable.rat_zodiac); //i have images in res/raw folder
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Ox");
-        hm.put(IMGKEY, R.drawable.ox_zodiac);
-        hm.put(RATINGKEY, 1);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Tiger");
-        hm.put(IMGKEY, R.drawable.tiger_zodiac);
-        hm.put(RATINGKEY,3);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Rabit");
-        hm.put(IMGKEY, R.drawable.rabbit_zodiac);
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-        
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Dragon");
-        hm.put(IMGKEY, R.drawable.dragon_zodiac); //i have images in res/raw folder
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Snake");
-        hm.put(IMGKEY, R.drawable.snake_zodiac);
-        hm.put(RATINGKEY, 1);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Horse");
-        hm.put(IMGKEY, R.drawable.horse_zodiac);
-        hm.put(RATINGKEY,3);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Sheep");
-        hm.put(IMGKEY, R.drawable.sheep_zodiac);
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-        
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Monkey");
-        hm.put(IMGKEY, R.drawable.monkey_zodiac); //i have images in res/raw folder
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Rooster");
-        hm.put(IMGKEY, R.drawable.rooster_zodiac);
-        hm.put(RATINGKEY, 1);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Dog");
-        hm.put(IMGKEY, R.drawable.dog_zodiac);
-        hm.put(RATINGKEY,3);
-        horoscopes_hashmap.add(hm);
-         
-        hm = new HashMap<String, Object>();
-        hm.put(TITLEKEY, "Pig");
-        hm.put(IMGKEY, R.drawable.pig_zodiac);
-        hm.put(RATINGKEY, 2);
-        horoscopes_hashmap.add(hm);
-    
+  
+        hashmapElement("Rat",R.drawable.rat_zodiac, 2);
+        hashmapElement("Ox",R.drawable.ox_zodiac, 5);
+        hashmapElement("Tiger",R.drawable.tiger_zodiac, 3);
+        hashmapElement("Rabit",R.drawable.rabbit_zodiac, 8);
+        hashmapElement("Dragon",R.drawable.dragon_zodiac, 1);
+        hashmapElement("Snake",R.drawable.snake_zodiac, 2);
+        hashmapElement("Horse",R.drawable.horse_zodiac, 4);
+        hashmapElement("Sheep",R.drawable.sheep_zodiac, 7);
+        hashmapElement("Monkey",R.drawable.monkey_zodiac, 3);
+        hashmapElement("Rooster",R.drawable.rooster_zodiac, 2);
+        hashmapElement("Dog",R.drawable.dog_zodiac, 9);
+        hashmapElement("Pig",R.drawable.pig_zodiac, 10);
         listView.setAdapter(new myListAdapter(horoscopes_hashmap,this));
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
     }
@@ -138,10 +74,11 @@ public class HoroscopesList extends Activity {
     private class myListAdapter extends BaseAdapter{
         private ArrayList<HashMap<String, Object>> Horoscopes;
         private LayoutInflater mInflater;
-        public myListAdapter(ArrayList<HashMap<String, Object>> books, Context context){
-            Horoscopes = books;
+        public myListAdapter(ArrayList<HashMap<String, Object>> horoscopes, Context context){
+            Horoscopes = horoscopes;
             mInflater = LayoutInflater.from(context);
         }
+        
         @Override
         public int getCount() {
             // TODO Auto-generated method stub
@@ -160,20 +97,13 @@ public class HoroscopesList extends Activity {
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-            // A ViewHolder keeps references to children views to avoid unneccessary calls
-            // to findViewById() on each row.
             ViewHolder holder;
-            // When convertView is not null, we can reuse it directly, there is no need
-            // to reinflate it. We only inflate a new View when the convertView supplied
-            // by ListView is null
              if (convertView == null) {
                  convertView = mInflater.inflate(R.layout.listbox, null);
                  // Creates a ViewHolder and store references to the two children views
                  // we want to bind data to.
                  holder = new ViewHolder();
                  holder.v = (TextView) convertView.findViewById(R.id.title);
-            //     holder.v1 = (TextView) convertView.findViewById(R.id.text2);
                  holder.icon = (ImageView) convertView.findViewById(R.id.img);
                  holder.rating = (RatingBar)convertView.findViewById(R.id.star);
                  convertView.setTag(holder);
@@ -197,6 +127,14 @@ public class HoroscopesList extends Activity {
          
     }
  
-     
+     public void hashmapElement(String title, int img, int rate)
+     {
+    	 HashMap<String, Object> hm;
+    	 hm = new HashMap<String, Object>();
+         hm.put(TITLEKEY, title);
+         hm.put(IMGKEY, img);
+         hm.put(RATINGKEY,rate);
+         horoscopes_hashmap.add(hm);
+     }
 
 }
