f1dbc93eb0ff1153a7c0c95555f9dc372f285a30
ahmedmostafa343 <ahmedmostafa343@gmail.com>
2011-10-11 01:46:17 +0000

    Added images to Table view. Followed best practices when moving data from the Grid Activity to the Table Activity using the bundle extras.

--- Sheep/Horoscopes Experiment/src/com/example/Lab1Activity.java
+++ Sheep/Horoscopes Experiment/src/com/example/Lab1Activity.java
@@ -3,31 +3,30 @@ package com.example;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Lab1Activity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle bundle = this.getIntent().getExtras();
-		String input = bundle.getString("param1");
-        int id = 0;
-        if (input.equals("Horse") )
-			id = R.array.horse_array;
-		else if (input.equals("Rabbit"))
-			id = R.array.rabbit_array;
-		else if (input.equals("Sheep"))
-			id = R.array.goat_array;
-		else if (input.equals("Snake"))
-			id = R.array.snake_array;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.table_layout);
+
+		Bundle bundle = this.getIntent().getExtras();
 		Resources res = getResources();
-		String[] horoscope = res.getStringArray(id);
-        
-        setContentView(R.layout.table_layout);
-        TextView titleView = (TextView) findViewById(R.id.title);
-        TextView descriptionView = (TextView) findViewById(R.id.description);
-        titleView.setText(horoscope[0]);
-        descriptionView.setText(horoscope[1]);
-    }
+
+		String[] horo_names = res.getStringArray(R.array.horo_array);
+		String[] horo_descriptions = res
+				.getStringArray(R.array.horo_description_array);
+
+		TextView titleView = (TextView) findViewById(R.id.title);
+		TextView descriptionView = (TextView) findViewById(R.id.description);
+		ImageView imageView = (ImageView) findViewById(R.id.image);
+
+		titleView.setText(horo_names[bundle.getInt("title_position")]);
+		descriptionView.setText(horo_descriptions[bundle
+				.getInt("description_position")]);
+		imageView.setImageResource(bundle.getInt("image_id"));
+	}
 }
