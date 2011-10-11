3df9c4ada9bd6e19e6b32b2bbb97d6f4984e7d35
dalia9123 <dalia9123@gmail.com>
2011-10-11 19:01:50 +0000

    progress bar

--- Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeGrid.java
+++ Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeGrid.java
@@ -1,12 +1,14 @@
 package com.horoscope;
 
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -16,43 +18,78 @@ import android.widget.AdapterView.OnItemClickListener;
  */
 public class HoroscopeGrid extends Activity {
 	/** Called when the activity is first created. */
+	
+	GridView gridview;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		setContentView(R.layout.grid_progress);
-		//the progress bar
-		ProgressBar progressHorizontal = (ProgressBar) findViewById(R.id.progressbar1);
+		setContentView(R.layout.grid);
 		
-		
-		/*
-		 * Adding the grid for the view
-		 */
-		GridView gridview = (GridView) findViewById(R.id.gridview);
-		gridview.setAdapter(new ImageAdapter(this));
-	
+		//show a progress bar
+		AsyncTask<Void, Void, Void> read = new AsyncTask<Void, Void, Void>() {
+		    ProgressDialog progress;
 
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				
-				/*
-				 * Link auto-complete textbox to list of horoscopes
+		    @Override
+		    protected void onPreExecute() {
+		    	progress = new ProgressDialog(HoroscopeGrid.this);
+		    	progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		        progress.setMessage("Loading. Please wait..."); 
+		        progress.setIndeterminate(true);
+		        progress.show();
+		        super.onPreExecute();
+		    }
+
+		    
+
+		    @Override
+		    protected void onPostExecute(Void result) {
+		        super.onPostExecute(result);
+		       
+
+	     		 /*
+				 * Adding the grid for the view
 				 */
-				
-				Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
-				i.putExtra("pos", position%12);
-				i.putExtra("title", ((String)((TextView)v).getTag()).toString());
-				startActivity(i);
-				
+				gridview = (GridView) findViewById(R.id.gridview);
+	     		gridview.setAdapter(new ImageAdapter(HoroscopeGrid.this));
+	     	
+
+	     		gridview.setOnItemClickListener(new OnItemClickListener() {
+	     			@Override
+	     			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	     				
+	     				/*
+	     				 * Link auto-complete textbox to list of horoscopes
+	     				 */
+	     				
+	     				Intent i = new Intent(HoroscopeGrid.this, HoroscopeDetails.class);
+	     				i.putExtra("pos", position%12);
+	     				i.putExtra("title", ((String)((TextView)v).getTag()).toString());
+	     				startActivity(i);
+	     				
+	     			}
+	     		});
+		      
+	     		//hide bar when done
+	     		progress.dismiss();
+	     		
+	     		
+		    }
+
+
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				// TODO Auto-generated method stub
+
+				return null;
 			}
-		});
-		//hide bar when done
-		progressHorizontal.setVisibility(View.INVISIBLE);
-		//display gridview
-		gridview.setVisibility(View.VISIBLE);
+		};
+		read.execute();
 		
-       
+	
+		
+		
 	}
 	
 }
