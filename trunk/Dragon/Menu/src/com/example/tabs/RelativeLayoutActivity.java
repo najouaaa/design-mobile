54811cbd04f1062bfd2763eab6b9f69dc3edaf2b
emy.mansour <emy.mansour@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 16:45:39 +0000

    updating the RelativeLayoutActivity.java to include a spinner and a ViewStub

--- trunk/Dragon/Menu/src/com/example/tabs/RelativeLayoutActivity.java
+++ trunk/Dragon/Menu/src/com/example/tabs/RelativeLayoutActivity.java
@@ -1,48 +1,94 @@
 package com.example.tabs;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class RelativeLayoutActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative);
-    }
+public class RelativeLayoutActivity extends Activity{
+	
+	boolean inflated = false;
+	ViewStub stub;
+    View inflatedView;
+    TextView text;
+    ImageView image;
     
- 	public void myClickHandler(View view) {
- 		Intent intent;
- 		switch (view.getId())
- 		{
- 		case R.id.horse:
- 			LinearActivity.the_id=1;
-  			intent = new Intent().setClass(RelativeLayoutActivity.this, LinearActivity.class);
-  			startActivity(intent);
- 			break;
- 		case R.id.snake:
- 			LinearActivity.the_id=3;
-  			intent = new Intent().setClass(RelativeLayoutActivity.this, LinearActivity.class);
-  			startActivity(intent);
- 			break;
- 		case R.id.dragon:
- 			LinearActivity.the_id=0;
- 			intent = new Intent().setClass(RelativeLayoutActivity.this, LinearActivity.class);
- 			startActivity(intent);
- 			break;
- 		case R.id.rabbit:
- 			LinearActivity.the_id=2;
-  			intent = new Intent().setClass(RelativeLayoutActivity.this, LinearActivity.class);
-  			startActivity(intent);
- 			break;
- 		case R.id.sheep:
- 			LinearActivity.the_id=4;
-  			intent = new Intent().setClass(RelativeLayoutActivity.this, LinearActivity.class);
-  			startActivity(intent);
- 			break;
- 		}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.relative);
+
+	    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.horoscopes, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner.setAdapter(adapter);
+	    spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
 	}
+	
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+	    public void onItemSelected(AdapterView<?> parent,
+	        View view, int pos, long id) {
+	        
+	    	if(!inflated)
+	    	{
+	    		stub = (ViewStub) findViewById(R.id.stub) ;
+    			inflatedView = stub.inflate();
+    			text = (TextView) inflatedView.findViewById(R.id.description);
+    			image = (ImageView) inflatedView.findViewById(R.id.img);
+    			inflated = true;
+	    	}
+	    	
+	    	switch(pos){
+	    		
+	    		case 0:
+	    			text.setText(" ");
+	    			image.setVisibility(ImageView.INVISIBLE);
+	    		break;
+	    		
+	    		case 1:
+	    			image.setVisibility(ImageView.VISIBLE);
+	    	        text.setText(R.string.HorseDescription);
+	    	        image.setImageResource(R.drawable.horse);
+	    		break;
+	    		
+	    		case 2:
+	    	        text.setText(R.string.DragonDescription);
+	    	        image.setVisibility(ImageView.VISIBLE);
+	    	        image.setImageResource(R.drawable.dragon);
+	    		break;
+	    		
+	    		case 3:
+	    	        text.setText(R.string.SnakeDescription);
+	    	        image.setVisibility(ImageView.VISIBLE);
+	    	        image.setImageResource(R.drawable.snake);
+		    	break;
+		    		
+	    		case 4:
+	    	        text.setText(R.string.SheepDescription);
+	    	        image.setVisibility(ImageView.VISIBLE);
+	    	        image.setImageResource(R.drawable.sheep);
+		    	break;
+		    		
+	    		case 5:
+	    	        text.setText(R.string.RabbitDescription);
+	    	        image.setVisibility(ImageView.VISIBLE);
+	    	        image.setImageResource(R.drawable.rabbit);
+		    	break;
+	    	}
+	      
+	    }
+
+	    public void onNothingSelected(AdapterView parent) {
+	      // Do nothing.
+	    }
+	}
+
 }
-    
