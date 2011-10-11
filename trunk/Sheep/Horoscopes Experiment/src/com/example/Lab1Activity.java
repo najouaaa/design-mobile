0e12ddf4c43af1f1e8f99fba007dde1f606ec4d0
ahmedmostafa343@gmail.com <ahmedmostafa343@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 17:12:10 +0000

    Task resolved: Add next/back buttons in each of the detailed views for the horoscope

--- trunk/Sheep/Horoscopes Experiment/src/com/example/Lab1Activity.java
+++ trunk/Sheep/Horoscopes Experiment/src/com/example/Lab1Activity.java
@@ -1,9 +1,12 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,9 +16,9 @@ public class Lab1Activity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.table_layout);
-		
+
 		Bundle extras = getIntent().getExtras();
-		int position = extras.getInt("position");
+		final int position = extras.getInt("position");
 		Resources res = getResources();
 		String[] horo_names = res.getStringArray(R.array.horo_array);
 		String[] horo_descriptions = res
@@ -27,5 +30,29 @@ public class Lab1Activity extends Activity {
 		titleView.setText(horo_names[position]);
 		descriptionView.setText(horo_descriptions[position]);
 		imageView.setImageResource(ImageMapper.findBig(position));
+
+		final Button next = (Button) findViewById(R.id.next);
+		next.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				Intent intent = new Intent(getApplicationContext(),
+						Lab1Activity.class);
+				int newPosition = (position >= 3) ? 0 : position + 1;
+				intent.putExtra("position", newPosition);
+				startActivity(intent);
+			}
+		});
+
+		final Button back = (Button) findViewById(R.id.back);
+		back.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				Intent intent = new Intent(getApplicationContext(),
+						Lab1Activity.class);
+				int newPosition = (position <= 0) ? 3 : position - 1;
+				intent.putExtra("position", newPosition);
+				startActivity(intent);
+			}
+		});
 	}
 }
