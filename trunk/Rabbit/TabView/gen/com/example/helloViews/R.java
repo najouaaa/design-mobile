ad4f7440be085402a4f2d7ebc762a9bf7ae460a1
magued.george1990@gmail.com <magued.george1990@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 18:08:37 +0000

    gen folder

--- trunk/Rabbit/TabView/gen/com/example/helloViews/R.java
+++ trunk/Rabbit/TabView/gen/com/example/helloViews/R.java
@@ -31,47 +31,54 @@ public final class R {
         public static final int tiger=0x7f020011;
     }
     public static final class id {
-        public static final int dragon=0x7f050006;
-        public static final int gridview=0x7f050000;
-        public static final int icon=0x7f050001;
-        public static final int ox=0x7f050004;
-        public static final int rabbit=0x7f050005;
-        public static final int rat=0x7f050007;
-        public static final int textview1=0x7f050002;
-        public static final int textview2=0x7f050003;
-        public static final int tiger=0x7f050008;
+        public static final int dragon=0x7f05000b;
+        public static final int gridview=0x7f050005;
+        public static final int icon=0x7f050006;
+        public static final int ox=0x7f050009;
+        public static final int rabbit=0x7f05000a;
+        public static final int rat=0x7f05000c;
+        public static final int ratingbar1=0x7f050000;
+        public static final int ratingbar2=0x7f050001;
+        public static final int ratingbar3=0x7f050002;
+        public static final int ratingbar4=0x7f050003;
+        public static final int ratingbar5=0x7f050004;
+        public static final int textview1=0x7f050007;
+        public static final int textview2=0x7f050008;
+        public static final int tiger=0x7f05000d;
     }
     public static final class layout {
-        public static final int grid_view=0x7f030000;
-        public static final int linear=0x7f030001;
-        public static final int list_view=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int relative_view=0x7f030004;
-        public static final int table_view=0x7f030005;
+        public static final int custom_view=0x7f030000;
+        public static final int grid_view=0x7f030001;
+        public static final int linear=0x7f030002;
+        public static final int list_view=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int relative_view=0x7f030005;
+        public static final int table_view=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int dragon_desc=0x7f040015;
+        public static final int custom_view=0x7f040006;
+        public static final int dragon_desc=0x7f040016;
         public static final int grid_view=0x7f040003;
-        public static final int h_dog=0x7f040010;
-        public static final int h_dragon=0x7f04000a;
-        public static final int h_horse=0x7f04000c;
-        public static final int h_monkey=0x7f04000e;
-        public static final int h_ox=0x7f040007;
-        public static final int h_pig=0x7f040011;
-        public static final int h_rabbit=0x7f040009;
-        public static final int h_rat=0x7f040006;
-        public static final int h_rooster=0x7f04000f;
-        public static final int h_sheep=0x7f04000d;
-        public static final int h_snake=0x7f04000b;
-        public static final int h_tiger=0x7f040008;
+        public static final int h_dog=0x7f040011;
+        public static final int h_dragon=0x7f04000b;
+        public static final int h_horse=0x7f04000d;
+        public static final int h_monkey=0x7f04000f;
+        public static final int h_ox=0x7f040008;
+        public static final int h_pig=0x7f040012;
+        public static final int h_rabbit=0x7f04000a;
+        public static final int h_rat=0x7f040007;
+        public static final int h_rooster=0x7f040010;
+        public static final int h_sheep=0x7f04000e;
+        public static final int h_snake=0x7f04000c;
+        public static final int h_tiger=0x7f040009;
         public static final int hello=0x7f040000;
         public static final int list_view=0x7f040004;
-        public static final int ox_desc=0x7f040012;
-        public static final int rabbit_desc=0x7f040014;
-        public static final int rat_desc=0x7f040016;
+        public static final int ox_desc=0x7f040013;
+        public static final int rabbit_desc=0x7f040015;
+        public static final int rat_desc=0x7f040017;
         public static final int relative_view=0x7f040002;
         public static final int table_view=0x7f040005;
-        public static final int tiger_desc=0x7f040013;
+        public static final int tiger_desc=0x7f040014;
     }
 }
