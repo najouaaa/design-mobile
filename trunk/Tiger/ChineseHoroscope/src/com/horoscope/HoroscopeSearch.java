d1d253e3b6471bb25e5b77bdba1ad92aed728829
kamasheto <kamasheto@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 20:48:43 +0000

    Update issue 75 Status: started
    Spinner implemented, still missing small tweak regarding initial fire.

--- trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeSearch.java
+++ trunk/Tiger/ChineseHoroscope/src/com/horoscope/HoroscopeSearch.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 /**
  * @author kamasheto
@@ -20,7 +22,11 @@ public class HoroscopeSearch extends Activity {
 	/**
 	 * 
 	 */
-	AutoCompleteTextView textView;
+	// AutoCompleteTextView textView;
+
+	Spinner spinner;
+
+	boolean activateListener = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstance) {
@@ -28,72 +34,12 @@ public class HoroscopeSearch extends Activity {
 		setContentView(R.layout.search);
 		horoscopes = getResources().getStringArray(R.array.horoscope_array);
 
-		/*
-		 * Link autocomplete textbox to list of horoscopes
-		 */
-		textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_horoscope);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view, horoscopes);
-		textView.setThreshold(1);
-		textView.setAdapter(adapter);
-	}
-
-	/**
-	 * Clears search box every time activity is brought to focus
-	 */
-	protected void onResume() {
-		super.onResume();
-		textView.setText("");
-	}
-
-	/**
-	 * What to do when clicking ok? Search.
-	 * 
-	 * @param view
-	 */
-	public void onOkClick(View view) {
-		//
-		int position = 0;
-		/*
-		 * Are we searching for an empty box?
-		 */
-		if (textView.getText() == null) {
-			showMessage("Please enter a search query.");
-			return;
-		}
-
-		/*
-		 * Does this term exist in anything? For now we're searching case
-		 * insensitive, but do we want that?
-		 */
-		String query = textView.getText().toString();
-
-		/*
-		 * 
-		 */
-		if (query.length() == 0) {
-			showMessage("Please enter a search query.");
-			return;
-		}
-
-		boolean found = false;
-		for (String horoscope : horoscopes) {
-			if (horoscope.toLowerCase().contains(query.toLowerCase())) {
-				found = true;
-				query = horoscope;
-				break;
-			}
-			position++;
-		}
-
-		if (!found) {
-			showMessage("No matches found.");
-			return;
-		}
-
-		Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
-		i.putExtra("pos", position);
-		i.putExtra("title", query);
-		startActivity(i);
+		Spinner spinner = (Spinner) findViewById(R.id.spinner_horoscope);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.horoscope_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+		activateListener = true;
 	}
 
 	/**
@@ -104,5 +50,65 @@ public class HoroscopeSearch extends Activity {
 	 */
 	public void showMessage(String text) {
 		Toast.makeText(HoroscopeSearch.this, text, Toast.LENGTH_SHORT).show();
+	}
+
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+		public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+			if (!activateListener) {
+				return;
+			}
+			//
+			int position = 0;
+			/*
+			 * Are we searching for an empty box?
+			 */
+			// if (textView.getText() == null) {
+			// showMessage("Please enter a search query.");
+			// return;
+			// }
+
+			/*
+			 * Does this term exist in anything? For now we're searching case
+			 * insensitive, but do we want that?
+			 */
+			// String query = textView.getText().toString();
+			String query = parent.getItemAtPosition(pos).toString();
+
+			/*
+			 * 
+			 */
+			if (query.length() == 0) {
+				showMessage("Please enter a search query.");
+				return;
+			}
+			//
+			boolean found = false;
+			for (String horoscope : horoscopes) {
+				if (horoscope.toLowerCase().contains(query.toLowerCase())) {
+					found = true;
+					query = horoscope;
+					break;
+				}
+				position++;
+			}
+			//
+			if (!found) {
+				showMessage("No matches found.");
+				return;
+			}
+
+			Intent i = new Intent(getBaseContext(), HoroscopeDetails.class);
+			i.putExtra("pos", position);
+			i.putExtra("title", query);
+			startActivity(i);
+			// Toast.makeText(parent.getContext(), "The planet is " +
+			// parent.getItemAtPosition(pos).toString(),
+			// Toast.LENGTH_LONG).show();
+		}
+
+		public void onNothingSelected(AdapterView parent) {
+			// Do nothing.
+		}
 	}
 }
