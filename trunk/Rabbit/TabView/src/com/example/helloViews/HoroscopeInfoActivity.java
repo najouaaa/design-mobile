41c55672c553cc08c8e3434c5300fc21ddfbe39d
anwarower@gmail.com <anwarower@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-11 19:21:35 +0000

    Adding the feature : Add next/prev bar to the detailed view

--- trunk/Rabbit/TabView/src/com/example/helloViews/HoroscopeInfoActivity.java
+++ trunk/Rabbit/TabView/src/com/example/helloViews/HoroscopeInfoActivity.java
@@ -1,31 +1,74 @@
 package com.example.helloViews;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HoroscopeInfoActivity extends Activity {
     /** Called when the activity is first created. */
-    
+    int ids[]; 
+    static int currentIndex; 
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear);
-      
+        initIDs();
         // Get the intent to retrieve input between different activities
         Bundle extras = getIntent().getExtras();
         String animalValue="";
         if(extras !=null) {
             animalValue = extras.getString(TabViewActivity.ANIMAL_KEY);
         }// end if: extract info sent by the intent
-             
+        this.setCurrentIndex(animalValue);     
         this.setAnimal(animalValue);        
         
     }// end onCreate
     
-     
+    public void initIDs() {
+    	ids = new int [5];
+    	ids[0] = R.string.h_rabbit;
+    	ids[1] = R.string.h_dragon;
+    	ids[2] = R.string.h_ox;
+    	ids[3] = R.string.h_rat;
+    	ids[4] = R.string.h_tiger;		
+    }
+    
+    public void setCurrentIndex(String element) {
+    	for(int i = 0; i <= 4; i++) {
+    		String temp = getString(ids[i]);
+    		if(temp.equalsIgnoreCase(element)) {
+    			currentIndex = i; 
+    		}
+    	}
+    }
+    
+	public void onClickButton(View v) {
+		Button b = (Button) v;
+		// Generate intent to move between activities
+		Intent mIntent = new Intent(getApplicationContext(),
+				HoroscopeInfoActivity.class);
+		// Pass variable to detailed view activity using the intent
+		if (b.getId() == R.id.next) {
+			currentIndex++;
+			currentIndex = currentIndex % 5;
+			mIntent.putExtra(TabViewActivity.ANIMAL_KEY,
+					getString(ids[currentIndex]));
+		} else {
+			currentIndex += 4;
+			currentIndex = currentIndex % 5;
+			mIntent.putExtra(TabViewActivity.ANIMAL_KEY,
+					getString(ids[currentIndex]));
+		}
+
+		// Start the new activity
+		startActivity(mIntent);
+	}
+    
     public void setAnimal(String animal) {
     	
     	ImageView iv = (ImageView)findViewById(R.id.icon);
