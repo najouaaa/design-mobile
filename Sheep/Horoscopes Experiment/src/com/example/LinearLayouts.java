74188fbc62755b138fd7f31c3c1d7de8795249d8
loai1991 <loai1991@gmail.com>
2011-10-12 07:49:58 +0000

    Task 5: adding image, rating ad viewstub for the horoscopes friends

--- Sheep/Horoscopes Experiment/src/com/example/LinearLayouts.java
+++ Sheep/Horoscopes Experiment/src/com/example/LinearLayouts.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class LinearLayouts extends Activity {
@@ -17,38 +19,47 @@ public class LinearLayouts extends Activity {
 		setContentView(R.layout.linearlayout);
 
 		Bundle bundle = getIntent().getExtras();
-		
+
 		final int position = bundle.getInt("position");
+		float rating = (float) bundle.getDouble("rating");
 
 		TextView titleView = (TextView) findViewById(R.id.title);
 		TextView descriptionView = (TextView) findViewById(R.id.description);
 		ImageView imageView = (ImageView) findViewById(R.id.image);
+		RatingBar ratingBar = (RatingBar) findViewById(R.id.rateLinear);
+
 		Resources res = getResources();
-		
+
 		String[] horo_names = res.getStringArray(R.array.horo_array);
 		String[] horo_descriptions = res
 				.getStringArray(R.array.horo_description_array);
-		
+		String[] horo_friends = res.getStringArray(R.array.horo_friends);
+
 		titleView.setText(horo_names[position]);
 		descriptionView.setText(horo_descriptions[position]);
 		imageView.setImageResource(ImageMapper.findBig(position));
-		
+		ratingBar.setRating(rating);
+		ViewStub stub = (ViewStub) findViewById(R.id.stub);
+		TextView inflated = (TextView) stub.inflate();
+		inflated.setText("Friends : " + horo_friends[position]);
 		final Button next = (Button) findViewById(R.id.next);
 		next.setOnClickListener(new View.OnClickListener() {
-			
+
 			public void onClick(View view) {
-				Intent intent = new Intent(getApplicationContext(), LinearLayouts.class);
+				Intent intent = new Intent(getApplicationContext(),
+						LinearLayouts.class);
 				int newPosition = (position >= 3) ? 0 : position + 1;
 				intent.putExtra("position", newPosition);
 				startActivity(intent);
 			}
 		});
-		
+
 		final Button back = (Button) findViewById(R.id.back);
 		back.setOnClickListener(new View.OnClickListener() {
-			
+
 			public void onClick(View view) {
-				Intent intent = new Intent(getApplicationContext(), LinearLayouts.class);
+				Intent intent = new Intent(getApplicationContext(),
+						LinearLayouts.class);
 				int newPosition = (position <= 0) ? 3 : position - 1;
 				intent.putExtra("position", newPosition);
 				startActivity(intent);
