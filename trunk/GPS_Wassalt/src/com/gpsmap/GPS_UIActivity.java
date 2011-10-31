b350ce3a74fb9c7d01d6d34c6dc314641cba8a87
aya.skamel <aya.skamel@58fc6eb2-5516-efc4-609f-126101431d35>
2011-10-31 22:20:30 +0000

    UI Controls Activity.. Needs testing and needs to be connected to other activities

---
+++ trunk/GPS_Wassalt/src/com/gpsmap/GPS_UIActivity.java
@@ -0,0 +1,93 @@
+package com.gpsmap;
+
+import com.example.tabs.LinearActivity;
+import com.example.tabs.ListViewActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class GPS_UIActivity extends Activity implements View.OnClickListener{
+    /** Called when the activity is first created. */
+	
+	private boolean toggle = true;
+	private Button start_end;
+	private EditText frequency;
+	private EditText distance;
+	private Spinner type;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //************       Spinner      ***************
+        
+        type = (Spinner)findViewById(R.id.types);
+        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.avail_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        type.setAdapter(adapter);
+        
+        //************       Text Boxes   **************
+        
+        frequency = (EditText)findViewById(R.id.frequency);
+        distance = (EditText)findViewById(R.id.distance);
+        
+        //************        Button      **************
+        
+        start_end = (Button)findViewById(R.id.start_end);
+        start_end.setOnClickListener(this);
+
+    }
+	
+	public void onClick(View v) {
+		toggle = !toggle;		
+		String str = "";
+		if(toggle)
+		{
+			String typ = type.getSelectedItem().toString();
+			String freq = frequency.getText().toString();
+			String dist = distance.getText().toString();
+			
+			start_end.setText("Start");
+			str = "Starting "+typ+
+					" service with a frequency of "+freq+
+					" and distance "+dist+"!!";
+			
+			/*
+			 * Intent intent;
+			 * if(typ.equals("GPS")
+			 * 		intent = new Intent().setClass(GPS_UIActivity.this, [GPS Class]);
+			 * else
+			 * {
+			 * 		if(typ.equals("AGPS"))
+			 * 			intent = new Intent().setClass(GPS_UIActivity.this, [AGPS Class]);
+			 * 		else
+			 * 			intent = new Intent().setClass(GPS_UIActivity.this, [Skyhook Class]);
+			 * }
+			 * startActivity(intent);
+			*/
+			type.setEnabled(false);
+			frequency.setEnabled(false);
+			distance.setEnabled(false);
+		}
+		else
+		{
+			start_end.setText("End");
+			str = "Ending service!!";
+			type.setEnabled(true);
+			frequency.setEnabled(true);
+			distance.setEnabled(true);
+		}
+		Toast.makeText(this, str, 0).show();
+	}
+}
