44b6152518a1e7d83a08912a47db4d07de129ed6
mabouelfadl@gmail.com <mabouelfadl@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-11-01 18:45:51 +0000

    added display current location

--- trunk/Audio Tour Guide/Location/src/com/example/Location/MyMap.java
+++ trunk/Audio Tour Guide/Location/src/com/example/Location/MyMap.java
@@ -32,14 +32,37 @@ public class MyMap extends MapActivity {
         mapOverlays = mapView.getOverlays();
         drawable = this.getResources().getDrawable(R.drawable.androidmarker);
         itemizedoverlay = new HelloItemizedOverlay(drawable);
-        GeoPoint point = new GeoPoint(19240000,-99120000);
+        
+        displayCurrentPosition();
+    }
+	
+	public void displayCurrentPosition(){
+	
+		int latitude = (int) (getIntent().getDoubleExtra("latitude",-1)*1000000);
+		int longitude = (int)(getIntent().getDoubleExtra("longitude",-1)*1000000);
+		GeoPoint point = new GeoPoint(latitude,longitude);
         OverlayItem overlayitem = new OverlayItem(point, "", "");
         itemizedoverlay.addOverlay(overlayitem);
         mapOverlays.add(itemizedoverlay);
-    }
+		}
     
     @Override
     protected boolean isRouteDisplayed() {
         return false;
     }
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		displayCurrentPosition();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		displayCurrentPosition();
+	}
 }
