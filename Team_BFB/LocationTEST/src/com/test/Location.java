8bc31af7cfb3b98dbb3be070b34becf375e0b253
rawan.tkhalil <rawan.tkhalil@gmail.com>
2011-11-01 22:29:38 +0000

    Team_BFB -> writing the collected data to the file - fixed error

--- Team_BFB/LocationTEST/src/com/test/Location.java
+++ Team_BFB/LocationTEST/src/com/test/Location.java
@@ -13,17 +13,15 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.gps.test.R;
-import com.gps.test.editSettingsActivity;
+import com.test.R;
+import com.test.editSettingsActivity;
 import com.skyhookwireless.wps.IPLocation;
 import com.skyhookwireless.wps.IPLocationCallback;
 import com.skyhookwireless.wps.WPSAuthentication;
@@ -36,7 +34,7 @@ import com.skyhookwireless.wps.XPS;
 
 public class Location extends Activity {
 	Button btnStopService;
-	TextView txtMsg;
+	TextView textBoxView;
 	Intent intentMyService;
 	Intent Skyhook;
 	ComponentName service;
@@ -49,43 +47,31 @@ public class Location extends Activity {
 	private static final int DONE_MESSAGE = 3;
 
 	private final MyLocationCallback _callback = new MyLocationCallback();
-	boolean skyhook = false;
-
-	// @Override
-	// protected void onDestroy() {
-	// try {
-	// stopService(intentMyService);
-	// unregisterReceiver(receiver);
-	// } catch (Exception e) {
-	// Log.e("MAIN-DESTROY>>>", e.getMessage());
-	// }
-	// Log.e("MAIN-DESTROY>>>", "Adios");
-	// }
+	boolean skyhook = true;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		txtMsg = (TextView) findViewById(R.id.txtMsg);
-
-		// setContentView(R.layout.main);
+		textBoxView = (TextView) findViewById(R.id.txtMsg);
 		if (skyhook) {
 			setUIHandler();
 			XPS wps = new XPS(this);
 			WPSAuthentication auth = new WPSAuthentication("salma_7amed",
 					"German university in cairo");
-			// wps.getIPLocation(auth,WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,_callback);
-			wps.getLocation(auth,
+			wps.getIPLocation(auth,
 					WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,
 					_callback);
 
+			// wps.getLocation(auth,
+			// WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,
+			// _callback);
+
 		} else {
-			// initiate the service
 			intentMyService = new Intent(this, MyGpsService.class);
 			service = startService(intentMyService);
-			txtMsg.setText("MyGpsService started - (see DDMS Log)");
-			// register & define filter for local listener
+			textBoxView.setText("MyGpsService started - (see DDMS Log)");
 			IntentFilter mainFilter = new IntentFilter(GPS_FILTER);
 			receiver = new MyMainLocalReceiver();
 			registerReceiver(receiver, mainFilter);
@@ -94,7 +80,7 @@ public class Location extends Activity {
 				public void onClick(View v) {
 					try {
 						stopService(new Intent(intentMyService));
-						txtMsg.setText("After stoping Service: \n"
+						textBoxView.setText("After stopping Service: \n"
 								+ service.getClassName());
 						btnStopService.setText("Finished");
 						btnStopService.setClickable(false);
@@ -105,27 +91,29 @@ public class Location extends Activity {
 			});
 		}
 		final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.serviceToggle);
-        togglebutton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-                if (togglebutton.isChecked()) {
-                	intentMyService.putExtra("minDist", getIntent().getIntExtra("minDist", 50));
-                	intentMyService.putExtra("freq", getIntent().getIntExtra("freq", 10000));
-                	service = startService(intentMyService);
-                	
-                } else {
-                    stopService(new Intent(intentMyService) );
-                }
-            }
-        });
-	}
-	 public void openSettings(View view)
-	  {
-		  Intent i = new Intent(getBaseContext(), editSettingsActivity.class);
-		  startActivity(i);
-	  }
-	private void setUIHandler() {
+		togglebutton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Perform action on clicks
+				if (togglebutton.isChecked()) {
+					intentMyService.putExtra("minDist", getIntent()
+							.getIntExtra("minDist", 50));
+					intentMyService.putExtra("freq",
+							getIntent().getIntExtra("freq", 10000));
+					service = startService(intentMyService);
 
+				} else {
+					stopService(new Intent(intentMyService));
+				}
+			}
+		});
+	}
+
+	public void openSettings(View view) {
+		Intent i = new Intent(getBaseContext(), editSettingsActivity.class);
+		startActivity(i);
+	}
+
+	private void setUIHandler() {
 		_handler = new Handler() {
 			@Override
 			public void handleMessage(final Message msg) {
@@ -135,13 +123,13 @@ public class Location extends Activity {
 					// final Location location = (Location) msg.obj;
 					// Log.e("location msg","ay kalam");
 					// txtMsg.setText("yarab");
-					txtMsg.setText(msg.obj.toString());
+					textBoxView.setText(msg.obj.toString());
 					Log.e("location msg", "ay kalam");
 
 					return;
 				case ERROR_MESSAGE:
 					Log.e("error msg", ((WPSReturnCode) msg.obj).name());
-					txtMsg.setText(((WPSReturnCode) msg.obj).name());
+					textBoxView.setText(((WPSReturnCode) msg.obj).name());
 					return;
 				case DONE_MESSAGE:
 					Log.e("end", "false");
@@ -174,24 +162,41 @@ public class Location extends Activity {
 		}
 
 		public void handleIPLocation(final IPLocation location) {
-
-			Log.e("location msg", "handle");
+			Log.e("location msg", "Handle IP Location");
 			// send a message to display the location
 			String x = " lat: " + location.getLatitude() + " " + " lon: "
 					+ location.getLongitude();
 			_handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
-
+			writeToFile(location.getLongitude(), location.getLatitude());
 		}
 
 		public void handleWPSLocation(final WPSLocation location) {
-			Log.e("location msg", "handlewps");
-
+			Log.e("location msg", "Handle WPS Location");
 			// send a message to display the location
 			String x = " lat: " + location.getLatitude() + " " + " lon: "
 					+ location.getLongitude();
 			_handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
-
+			writeToFile(location.getLongitude(), location.getLatitude());
 		}
+	}
+
+	private void writeToFile(double longitude, double latitude) {
+		String msg = "Service Type: " /* + serviceType */
+				+ "\nMin Distance: " /* + minDist */
+				+ "\nFrequency: " /* + freq */
+				+ "\nLongitude: " + longitude + "\nLatitude: " + latitude
+				+ "\n------------------------------------------\n";
+		try {
+			FileOutputStream fos = openFileOutput("comparison.txt", MODE_APPEND);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			osw.write(msg);
+			osw.flush();
+			osw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	class MyMainLocalReceiver extends BroadcastReceiver {
@@ -199,44 +204,9 @@ public class Location extends Activity {
 		public void onReceive(Context localContext, Intent callerIntent) {
 			double latitude = callerIntent.getDoubleExtra("latitude", -1);
 			double longitude = callerIntent.getDoubleExtra("longitude", -1);
-			Log.e("MAIN>>>", Double.toString(latitude));
-			Log.e("MAIN>>>", Double.toString(longitude));
-			String msg = "Service Type: " /* + serviceType */
-					+ "\nMin Distance: " /* + minDist */+ "\nFrequency: " /*
-																		 * +
-																		 * freq
-																		 */
-					+ "\nLongitude: " + longitude + "\nLatitude: " + latitude
-					+ "\n------------------------------------------\n";
-			texting(msg);
-			try {
-				FileOutputStream fos = openFileOutput("comparison.txt",
-						MODE_APPEND);
-				OutputStreamWriter osw = new OutputStreamWriter(fos);
-
-				osw.write(msg);
-				osw.flush();
-				osw.close();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			Log.e("onReceive - Latitude", Double.toString(latitude));
+			Log.e("onReceive - Longitude", Double.toString(longitude));
+			writeToFile(longitude, latitude);
 		}
-
-		private void texting(String msg) {
-			try {
-				SmsManager smsMgr = SmsManager.getDefault();
-				// Parameter of sendTextMessage are:
-				// destinationAddress, senderAddress,
-				// text, sentIntent, deliveryIntent)
-				// ----------------------------------------------------------
-				smsMgr.sendTextMessage("5556", "5551234", "Please meet me at: "
-						+ msg, null, null);
-			} catch (Exception e) {
-				Toast.makeText(getBaseContext(), "texting\n" + e.getMessage(),
-						1).show();
-			}
-		}// texting
-
 	}
 }
