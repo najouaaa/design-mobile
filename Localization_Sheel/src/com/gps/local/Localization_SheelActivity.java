981941c096bf886c7c7bde7b82cca2ec4c3bde90
hossam.amer12 <hossam.amer12@gmail.com>
2011-11-01 00:20:48 +0000

    Adding the Sheel_m3aaya localization project ... API version: 1.6

---
+++ Localization_Sheel/src/com/gps/local/Localization_SheelActivity.java
@@ -0,0 +1,13 @@
+package com.gps.local;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Localization_SheelActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
