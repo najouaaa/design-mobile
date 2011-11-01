ec2684a1c470331ff61ca53bd695a49a659520f3
rawan.tkhalil@gmail.com <rawan.tkhalil@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-11-01 22:52:45 +0000

    Team_BFB -> writing the collected data to the file.

--- trunk/Team_BFB/LocationTEST/src/com/test/MyGpsService.java
+++ trunk/Team_BFB/LocationTEST/src/com/test/MyGpsService.java
@@ -28,29 +28,29 @@ public class MyGpsService extends Service {
 	@Override
 	public void onStart(Intent intent, int startId) {
 		Log.e("<<MyGpsService-onStart>>", "I am alive-GPS!");
-		// we place the slow work of the service in its own thread so the 
-		// response we send our caller who run a "startService(...)" method 
+		// we place the slow work of the service in its own thread so the
+		// response we send our caller who run a "startService(...)" method
 		// gets a quick OK from us.
 		triggerService = new Thread(new Runnable() {
-		public void run() {
-		try {
-		Looper.prepare();
-		// try to get your GPS location using the LOCATION.SERVIVE provider
-		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		// This listener will catch and disseminate location updates
-		myLocationListener = new GPSListener();
-		long minTime = 10000;  // frequency update: 10 seconds
-		float minDistance = 50;  // frequency update: 50 meter
-		lm.requestLocationUpdates( //request GPS updates
-		LocationManager.GPS_PROVIDER,
-		minTime, 
-		minDistance, 
-		myLocationListener);
-		Looper.loop();
-		} catch (Exception e) {
-		Log.e("MYGPS", e.getMessage() );
-		}
-		}// run
+			public void run() {
+				try {
+					Looper.prepare();
+					// try to get your GPS location using the LOCATION.SERVIVE
+					// provider
+					lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+					// This listener will catch and disseminate location updates
+					myLocationListener = new GPSListener();
+					long minTime = 10000; // frequency update: 10 seconds
+					float minDistance = 50; // frequency update: 50 meter
+					lm.requestLocationUpdates(
+							// request GPS updates
+							LocationManager.GPS_PROVIDER, minTime, minDistance,
+							myLocationListener);
+					Looper.loop();
+				} catch (Exception e) {
+					Log.e("MYGPS", e.getMessage());
+				}
+			}// run
 		});
 		triggerService.start();
 	}
@@ -60,47 +60,41 @@ public class MyGpsService extends Service {
 	LocationManager lm;
 	GPSListener myLocationListener;
 	boolean isRunning = true;
-	
-    
-	
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	  
+
 	class GPSListener implements LocationListener {
 		public void onLocationChanged(Location location) {
-		//capture location data sent by current provider
-		double latitude = location.getLatitude();
-		double longitude = location.getLongitude();
-		//assemble data bundle to be broadcasted
-		Intent myFilteredResponse = new Intent(GPS_FILTER);
-		myFilteredResponse.putExtra("latitude", latitude);
-		myFilteredResponse.putExtra("longitude", longitude);
-		Log.e(">>GPS_Service<<", "Lat:" + latitude + " lon:" + longitude);
-		//send the location data out
-		sendBroadcast(myFilteredResponse);
+			// capture location data sent by current provider
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+			// assemble data bundle to be broadcasted
+			Intent myFilteredResponse = new Intent(GPS_FILTER);
+			myFilteredResponse.putExtra("latitude", latitude);
+			myFilteredResponse.putExtra("longitude", longitude);
+			Log.e(">>GPS_Service<<", "Lat:" + latitude + " lon:" + longitude);
+			// send the location data out
+			sendBroadcast(myFilteredResponse);
 		}
-
 
 		public void onProviderDisabled(String arg0) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void onProviderEnabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			// TODO Auto-generated method stub
-			
-		}
-	}	  
-	
-	
-	  
-}
 
+		}
+	}
+
+}
