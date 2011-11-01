16c673aa4192661e80c0b83b613e449b8372066f
hadeer.diwan@gmail.com <hadeer.diwan@gmail.com@58fc6eb2-5516-efc4-609f-126101431d35>
2011-11-01 21:26:03 +0000

    Added the map View

---
+++ trunk/Team_BFB/MapView/src/com/example/hellomapview/HelloItemizedOverlay.java
@@ -0,0 +1,33 @@
+package com.example.hellomapview;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class HelloItemizedOverlay extends ItemizedOverlay {
+	
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	
+	public HelloItemizedOverlay(Drawable arg0) {
+		super(boundCenterBottom(arg0));
+		// TODO Auto-generated constructor stub
+	}
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+}
---
+++ trunk/Team_BFB/MapView/src/com/example/hellomapview/HelloMapView.java
@@ -0,0 +1,51 @@
+package com.example.hellomapview;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.hardware.GeomagneticField;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class HelloMapView extends MapActivity {
+	LinearLayout linearLayout;
+	MapView mapView;
+	List<Overlay> mapOverlays;
+	Drawable drawable;
+	HelloItemizedOverlay itemizedOverlay;
+	//GeoPoint point = new GeoPoint(19240000,-99120000);
+	GeoPoint point = new GeoPoint(0,0);
+	//GeoPoint point = new GeoPoint((int)GPSTestActivity.latitude,(int)GPSTestActivity.longitude);
+	//GeoPoint point=new GeomagneticField(gdLatitudeDeg, gdLongitudeDeg, altitudeMeters, timeMillis)
+	OverlayItem overlayitem = new OverlayItem(point, "", "");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view1);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        itemizedOverlay = new HelloItemizedOverlay(drawable);
+        itemizedOverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedOverlay);
+  
+       
+        
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+}
