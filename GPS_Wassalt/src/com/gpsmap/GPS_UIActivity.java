73afb355f3a1e17e327c26f020bcff9d523bb93b
dalia9123 <dalia9123@gmail.com>
2011-11-01 09:06:58 +0000

    team wassalt: fixed bug in map view

--- GPS_Wassalt/src/com/gpsmap/GPS_UIActivity.java
+++ GPS_Wassalt/src/com/gpsmap/GPS_UIActivity.java
@@ -1,7 +1,5 @@
 package com.gpsmap;
 
-import com.example.tabs.LinearActivity;
-import com.example.tabs.ListViewActivity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,17 +13,17 @@ import android.widget.Toast;
 
 public class GPS_UIActivity extends Activity implements View.OnClickListener{
     /** Called when the activity is first created. */
-	
-	private boolean toggle = true;
-	private Button start_end;
-	private EditText frequency;
-	private EditText distance;
-	private Spinner type;
-	
+        
+        private boolean toggle = true;
+        private Button start_end;
+        private EditText frequency;
+        private EditText distance;
+        private Spinner type;
+        
     @Override
     public void onCreate(Bundle savedInstanceState) {
         
-    	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_controls);
         
         //************       Spinner      ***************
@@ -48,46 +46,46 @@ public class GPS_UIActivity extends Activity implements View.OnClickListener{
         start_end.setOnClickListener(this);
 
     }
-	
-	public void onClick(View v) {
-		toggle = !toggle;		
-		String str = "";
-		if(toggle)
-		{
-			String typ = type.getSelectedItem().toString();
-			String freq = frequency.getText().toString();
-			String dist = distance.getText().toString();
-			
-			start_end.setText("Start");
-			str = "Starting "+typ+
-					" service with a frequency of "+freq+
-					" and distance "+dist+"!!";
-			
-			/*
-			 * Intent intent;
-			 * if(typ.equals("GPS")
-			 * 		intent = new Intent().setClass(GPS_UIActivity.this, [GPS Class]);
-			 * else
-			 * {
-			 * 		if(typ.equals("AGPS"))
-			 * 			intent = new Intent().setClass(GPS_UIActivity.this, [AGPS Class]);
-			 * 		else
-			 * 			intent = new Intent().setClass(GPS_UIActivity.this, [Skyhook Class]);
-			 * }
-			 * startActivity(intent);
-			*/
-			type.setEnabled(false);
-			frequency.setEnabled(false);
-			distance.setEnabled(false);
-		}
-		else
-		{
-			start_end.setText("End");
-			str = "Ending service!!";
-			type.setEnabled(true);
-			frequency.setEnabled(true);
-			distance.setEnabled(true);
-		}
-		Toast.makeText(this, str, 0).show();
-	}
+        
+        public void onClick(View v) {
+                toggle = !toggle;               
+                String str = "";
+                if(toggle)
+                {
+                        String typ = type.getSelectedItem().toString();
+                        String freq = frequency.getText().toString();
+                        String dist = distance.getText().toString();
+                        
+                        start_end.setText("Start");
+                        str = "Starting "+typ+
+                                        " service with a frequency of "+freq+
+                                        " and distance "+dist+"!!";
+                        
+                        /*
+                         * Intent intent;
+                         * if(typ.equals("GPS")
+                         *              intent = new Intent().setClass(GPS_UIActivity.this, [GPS Class]);
+                         * else
+                         * {
+                         *              if(typ.equals("AGPS"))
+                         *                      intent = new Intent().setClass(GPS_UIActivity.this, [AGPS Class]);
+                         *              else
+                         *                      intent = new Intent().setClass(GPS_UIActivity.this, [Skyhook Class]);
+                         * }
+                         * startActivity(intent);
+                        */
+                        type.setEnabled(false);
+                        frequency.setEnabled(false);
+                        distance.setEnabled(false);
+                }
+                else
+                {
+                        start_end.setText("End");
+                        str = "Ending service!!";
+                        type.setEnabled(true);
+                        frequency.setEnabled(true);
+                        distance.setEnabled(true);
+                }
+                Toast.makeText(this, str, 0).show();
+        }
 }
