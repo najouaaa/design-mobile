277f50a41e25a9507ac7f32f41c541beb6cf9cb8
rawan.tkhalil <rawan.tkhalil@gmail.com>
2011-11-01 20:49:11 +0000

    Team_BFB -> writing the collected data to the file.

--- Team_BFB/LocationTEST/src/com/test/Location.java
+++ Team_BFB/LocationTEST/src/com/test/Location.java
@@ -1,5 +1,9 @@
 package com.test;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import com.skyhookwireless.wps.*;
 
 import android.app.Activity;
@@ -29,23 +33,23 @@ public class Location extends Activity {
 	String GPS_FILTER = "guc.action.GPS_LOCATION";
 	private Handler _handler;
 	boolean _stop = false;
-    private static final int LOCATION_MESSAGE = 1;
-    private static final int ERROR_MESSAGE = 2;
-    private static final int DONE_MESSAGE = 3;
-   
-    private final MyLocationCallback _callback = new MyLocationCallback();
-    boolean skyhook = true;
+	private static final int LOCATION_MESSAGE = 1;
+	private static final int ERROR_MESSAGE = 2;
+	private static final int DONE_MESSAGE = 3;
 
-//	@Override
-//	protected void onDestroy() {
-//		try {
-//			stopService(intentMyService);
-//			unregisterReceiver(receiver);
-//		} catch (Exception e) {
-//			Log.e("MAIN-DESTROY>>>", e.getMessage());
-//		}
-//		Log.e("MAIN-DESTROY>>>", "Adios");
-//	}
+	private final MyLocationCallback _callback = new MyLocationCallback();
+	boolean skyhook = true;
+
+	// @Override
+	// protected void onDestroy() {
+	// try {
+	// stopService(intentMyService);
+	// unregisterReceiver(receiver);
+	// } catch (Exception e) {
+	// Log.e("MAIN-DESTROY>>>", e.getMessage());
+	// }
+	// Log.e("MAIN-DESTROY>>>", "Adios");
+	// }
 
 	/** Called when the activity is first created. */
 	@Override
@@ -53,121 +57,113 @@ public class Location extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		txtMsg = (TextView) findViewById(R.id.txtMsg);
-		
-		
-		//setContentView(R.layout.main);
-		if(skyhook){
-		setUIHandler();	
-		XPS wps = new XPS(this);
-	    WPSAuthentication auth = new WPSAuthentication("salma_7amed", "German university in cairo");
-	//    wps.getIPLocation(auth,WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,_callback);
-	   wps.getLocation(auth,WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,_callback);
-	     
-	     }
-		else{
-		// initiate the service
-		intentMyService = new Intent(this, MyGpsService.class);
-		service = startService(intentMyService);
-		txtMsg.setText("MyGpsService started - (see DDMS Log)");
-		// register & define filter for local listener
-		IntentFilter mainFilter = new IntentFilter(GPS_FILTER);
-		receiver = new MyMainLocalReceiver();
-		registerReceiver(receiver, mainFilter);
-		btnStopService = (Button) findViewById(R.id.btnStopService);
-		btnStopService.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				try {
-					stopService(new Intent(intentMyService));
-					txtMsg.setText("After stoping Service: \n"
-							+ service.getClassName());
-					btnStopService.setText("Finished");
-					btnStopService.setClickable(false);
-				} catch (Exception e) {
-					Log.e("MYGPS", e.getMessage());
+
+		// setContentView(R.layout.main);
+		if (skyhook) {
+			setUIHandler();
+			XPS wps = new XPS(this);
+			WPSAuthentication auth = new WPSAuthentication("salma_7amed",
+					"German university in cairo");
+			// wps.getIPLocation(auth,WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,_callback);
+			wps.getLocation(auth,
+					WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,
+					_callback);
+
+		} else {
+			// initiate the service
+			intentMyService = new Intent(this, MyGpsService.class);
+			service = startService(intentMyService);
+			txtMsg.setText("MyGpsService started - (see DDMS Log)");
+			// register & define filter for local listener
+			IntentFilter mainFilter = new IntentFilter(GPS_FILTER);
+			receiver = new MyMainLocalReceiver();
+			registerReceiver(receiver, mainFilter);
+			btnStopService = (Button) findViewById(R.id.btnStopService);
+			btnStopService.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					try {
+						stopService(new Intent(intentMyService));
+						txtMsg.setText("After stoping Service: \n"
+								+ service.getClassName());
+						btnStopService.setText("Finished");
+						btnStopService.setClickable(false);
+					} catch (Exception e) {
+						Log.e("MYGPS", e.getMessage());
+					}
 				}
-			}
-		});
+			});
 		}
 	}
 
-	 private void setUIHandler()
-    {
-		 
-        _handler = new Handler()
-        {
-            @Override
-            public void handleMessage(final Message msg)
-            {
-                switch (msg.what)
-                {
-                case LOCATION_MESSAGE:
-//                	Log.e("location msg","ay kalam");
-//                 final Location location = (Location) msg.obj;
-//                   Log.e("location msg","ay kalam");
-//                   txtMsg.setText("yarab");
-                	txtMsg.setText( msg.obj.toString());
-                	Log.e("location msg","ay kalam");
-                    
-                    return;
-                case ERROR_MESSAGE:
-                	Log.e("error msg",((WPSReturnCode) msg.obj).name());
-                  	txtMsg.setText(((WPSReturnCode) msg.obj).name());
-                    return;
-                case DONE_MESSAGE:
-                	Log.e("end","false");
-                    _stop = false;
-                }
-            }
-        };
-    }
+	private void setUIHandler() {
 
-	
-	 private class MyLocationCallback implements IPLocationCallback,WPSLocationCallback{
-		 
-     public void done()
-     {
-         // tell the UI thread to re-enable the buttons
-    	 Log.e("location msg","done");
-         _handler.sendMessage(_handler.obtainMessage(DONE_MESSAGE));
-     }
+		_handler = new Handler() {
+			@Override
+			public void handleMessage(final Message msg) {
+				switch (msg.what) {
+				case LOCATION_MESSAGE:
+					// Log.e("location msg","ay kalam");
+					// final Location location = (Location) msg.obj;
+					// Log.e("location msg","ay kalam");
+					// txtMsg.setText("yarab");
+					txtMsg.setText(msg.obj.toString());
+					Log.e("location msg", "ay kalam");
 
-     public WPSContinuation handleError(final WPSReturnCode error)
-     {
-    	 Log.e("location msg","error");
-         // send a message to display the error
-    	 
-         _handler.sendMessage(_handler.obtainMessage(ERROR_MESSAGE, error));
-         // return WPS_STOP if the user pressed the Stop button
-//         if (! _stop)
-//             return WPSContinuation.WPS_CONTINUE;
-//         else
-             return WPSContinuation.WPS_STOP;
-             
-     }
+					return;
+				case ERROR_MESSAGE:
+					Log.e("error msg", ((WPSReturnCode) msg.obj).name());
+					txtMsg.setText(((WPSReturnCode) msg.obj).name());
+					return;
+				case DONE_MESSAGE:
+					Log.e("end", "false");
+					_stop = false;
+				}
+			}
+		};
+	}
 
-     public void handleIPLocation(final IPLocation location)
-     {
-    	 
-    	 Log.e("location msg","handle");
-         // send a message to display the location
-    	 String x = " lat: " + location.getLatitude() + " " + " lon: "
+	private class MyLocationCallback implements IPLocationCallback,
+			WPSLocationCallback {
+
+		public void done() {
+			// tell the UI thread to re-enable the buttons
+			Log.e("location msg", "done");
+			_handler.sendMessage(_handler.obtainMessage(DONE_MESSAGE));
+		}
+
+		public WPSContinuation handleError(final WPSReturnCode error) {
+			Log.e("location msg", "error");
+			// send a message to display the error
+
+			_handler.sendMessage(_handler.obtainMessage(ERROR_MESSAGE, error));
+			// return WPS_STOP if the user pressed the Stop button
+			// if (! _stop)
+			// return WPSContinuation.WPS_CONTINUE;
+			// else
+			return WPSContinuation.WPS_STOP;
+
+		}
+
+		public void handleIPLocation(final IPLocation location) {
+
+			Log.e("location msg", "handle");
+			// send a message to display the location
+			String x = " lat: " + location.getLatitude() + " " + " lon: "
 					+ location.getLongitude();
-      _handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
-      
-     }
+			_handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
 
-     public void handleWPSLocation(final WPSLocation location)
-     {
-    	 Log.e("location msg","handlewps");
-         
-         // send a message to display the location
-    	 String x = " lat: " + location.getLatitude() + " " + " lon: "
-			+ location.getLongitude();
-    	 _handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
-         
-     }
- }
+		}
 
+		public void handleWPSLocation(final WPSLocation location) {
+			Log.e("location msg", "handlewps");
+
+			// send a message to display the location
+			String x = " lat: " + location.getLatitude() + " " + " lon: "
+					+ location.getLongitude();
+			_handler.sendMessage(_handler.obtainMessage(LOCATION_MESSAGE, x));
+
+		}
+	}
 
 	class MyMainLocalReceiver extends BroadcastReceiver {
 		@Override
@@ -176,11 +172,26 @@ public class Location extends Activity {
 			double longitude = callerIntent.getDoubleExtra("longitude", -1);
 			Log.e("MAIN>>>", Double.toString(latitude));
 			Log.e("MAIN>>>", Double.toString(longitude));
-			String msg = " lat: " + Double.toString(latitude) + " " + " lon: "
-					+ Double.toString(longitude);
-			txtMsg.append("\n" + msg);
-			// testing the SMS-texting feature
+			String msg = "Service Type: " /* + serviceType */
+					+ "\nMin Distance: " /* + minDist */+ "\nFrequency: " /*
+																		 * +
+																		 * freq
+																		 */
+					+ "\nLongitude: " + longitude + "\nLatitude: " + latitude
+					+ "\n------------------------------------------\n";
 			texting(msg);
+			try {
+				FileOutputStream fos = openFileOutput("comparison.txt",
+						MODE_APPEND);
+				OutputStreamFWriter osw = new OutputStreamWriter(fOut);
+
+				osw.write(msg);
+				osw.flush();
+				osw.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 		private void texting(String msg) {
