c4ab8b5d5c4e4230d07d3da3f7ae7a853548ad7c
ahmedmostafa343 <ahmedmostafa343@gmail.com>
2011-11-01 13:51:45 +0000

    1. Added a map view to the SkyHook Project. 2. Added an overlay Android icon to the map that defaults to a hard-coded location. 3. Changed the main Activity to be SkyhookActivity.

--- Team:5aramly/Skyhook/src/com/kharamly/SkyhookActivity.java
+++ Team:5aramly/Skyhook/src/com/kharamly/SkyhookActivity.java
@@ -1,13 +1,36 @@
 package com.kharamly;
 
-import android.app.Activity;
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-public class SkyhookActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class SkyhookActivity extends MapActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+		HelloItemizedOverlay itemizedoverlay = new HelloItemizedOverlay(drawable);
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "", "");
+		itemizedoverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedoverlay);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
