7133ce9668e6bc672fc7c5ee8eefb581e6a1e93b
mabouelfadl <mabouelfadl@gmail.com>
2011-11-01 17:55:56 +0000

    added the marker

--- Audio Tour Guide/Location/src/com/example/Location/MyMap.java
+++ Audio Tour Guide/Location/src/com/example/Location/MyMap.java
@@ -1,24 +1,45 @@
 package com.example.Location;
 
-import android.os.Bundle;
-import android.widget.LinearLayout;
+import java.util.List;
 
+import com.example.Location.HelloItemizedOverlay;
+import com.example.Location.R;
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MyMap extends MapActivity {
 	
 	LinearLayout linearLayout;
 	MapView mapView;
+	List<Overlay> mapOverlays;
+	Drawable drawable;
+	HelloItemizedOverlay itemizedoverlay;
 
-	@Override
-	protected boolean isRouteDisplayed() {
-	    return false;
-	}
+
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
-	}
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        itemizedoverlay = new HelloItemizedOverlay(drawable);
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "", "");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 }
