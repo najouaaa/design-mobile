efad39ef955ff76a303101d8d21d75f6877688be
hossam.amer12 <hossam.amer12@gmail.com>
2011-11-01 12:12:06 +0000

    [Sheel M3aaya] Added the R class in the localization application

--- Localization_Sheel/gen/com/gps/local/R.java
+++ Localization_Sheel/gen/com/gps/local/R.java
@@ -8,16 +8,20 @@
 package com.gps.local;
 
 public final class R {
-    public static final class attr {
-    }
-    public static final class drawable {
-        public static final int icon=0x7f020000;
-    }
-    public static final class layout {
-        public static final int main=0x7f030000;
-    }
-    public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-    }
-}
+	Ê Êpublic static final class attr {
+	Ê Ê}
+	Ê Êpublic static final class drawable {
+	Ê Ê Ê Êpublic static final int androidmarker=0x7f020000;
+	Ê Ê Ê Êpublic static final int icon=0x7f020001;
+	Ê Ê}
+	Ê Êpublic static final class id {
+	Ê Ê Ê Êpublic static final int mainlayout=0x7f050000;
+	Ê Ê Ê Êpublic static final int mapview=0x7f050001;
+	Ê Ê}
+	Ê Êpublic static final class layout {
+	Ê Ê Ê Êpublic static final int main=0x7f030000;
+	Ê Ê}
+	Ê Êpublic static final class string {
+	Ê Ê Ê Êpublic static final int app_name=0x7f040001;
+	Ê Ê Ê Êpublic static final int hello=0x7f040000;
+	Ê Ê}
